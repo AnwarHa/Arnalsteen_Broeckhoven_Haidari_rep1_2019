3a016197872e57ff92a2a164afd465fdd2394a1e
Kalimath <mathieu_broe@yahoo.com>
2019-02-21 15:34:47 +0100

    sort setup

--- Shop/src/db/ShopDb.java
+++ Shop/src/db/ShopDb.java
@@ -2,17 +2,20 @@ package db;
 
 import domain.Product;
 
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ShopDb {
-    private Map<Integer, Product> productMap;
+    private Map<String, Product> productMap;
 
     public ShopDb(){
         productMap = new HashMap<>();
     }
 
-    public void addProduct(int id,Product product){
+    public void addProduct(Product product){
         //algoritme voor ID
 
         productMap.put(id, product);
@@ -23,4 +26,15 @@ public class ShopDb {
     public Map<Integer, Product> getProducten(){
         return productMap;
     }
+
+    public List<Product> sort(){
+        List<Product> sorted = new ArrayList<>();
+        for(Map.Entry<String,Product> m:productMap.entrySet()){
+
+        }
+        return sorted;
+
+    }
+
+
 }
