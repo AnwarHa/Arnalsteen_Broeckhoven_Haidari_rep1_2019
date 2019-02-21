24ae4ed1b4f6ce52bc25e81b78e700bd20a8eab8
Kalimath <mathieu_broe@yahoo.com>
2019-02-21 14:25:24 +0100

    voila

---
+++ Shop/src/domain/Movie.java
@@ -0,0 +1,18 @@
+package domain;
+
+public class Movie extends Product{
+    public Movie(String name, String id) {
+        super(name, id);
+    }
+
+    @Override
+    public double getPrice(int days){
+        double price = 0;
+            price = 5;
+            int daysLeft = days - 3;
+            if (daysLeft > 0) {
+                price += (daysLeft * 2);
+            }
+        return price;
+    }
+}
---
+++ Shop/src/domain/Shop.java
@@ -0,0 +1,46 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shop{
+    private List<Product> productDatabase = new ArrayList<>();
+
+    public Shop(String naam) {
+
+    }
+
+    public List<Product> getProducts() {
+        return productDatabase;
+    }
+
+    public void addProduct(Product product) {
+        productDatabase.add(product);
+    }
+
+    public double getPrice(String productName, int days) {
+        if(productName==null||productName.isEmpty()) throw new IllegalArgumentException("name of product is empty or doesn't exist");
+        double price = 0;
+        for(Product p : productDatabase){
+            if(p.getName().equals(productName)){
+                price = p.getPrice(days);
+            }
+        }
+        return price;
+    }
+
+    public Product getProduct(String productName) {
+        if(productName==null||productName.isEmpty()) throw new IllegalArgumentException("name of product is empty or doesn't exist");
+        Product product = null;
+        for(Product p : productDatabase){
+            if(p.getName().equals(productName)){
+                product = p;
+            }
+        }
+        return product;
+    }
+
+    public void close(){
+
+    }
+}
