6fc847a1b203df3e98859b709218dc31583a14d1
Unknown <sam.arnalsteen@gmail.com>
2019-02-28 15:35:18 +0100

    mirror geoptimaliseerd + codeercontext snap ik niks van

--- Geheimschrift/src/domain/CodeerContext.java
+++ Geheimschrift/src/domain/CodeerContext.java
@@ -1,7 +1,20 @@
 package domain;
 
 public class CodeerContext {
-    private String zin;
+    private String text;
+    private CodeStrategy codeStrategy;
 
-    
+
+    public CodeerContext(){
+
+    }
+
+    private void setText(String text){
+        this.text = text;
+    }
+
+    private void encode(){
+        codeStrategy.encode(this);
+    }
+
 }
