bbddb6c9c9acf01accc597d9f25e3d15ef921f41
Unknown <sam.arnalsteen@gmail.com>
2019-02-28 15:43:07 +0100

    nog iets aan codeercontext

--- Geheimschrift/src/domain/CodeerContext.java
+++ Geheimschrift/src/domain/CodeerContext.java
@@ -13,11 +13,15 @@ public class CodeerContext {
         this.text = text;
     }
 
+    public void setCodeStrategy(CodeStrategy codeStrategy){
+        
+    }
+
     private void encode(){
         codeStrategy.encode(this.text);
     }
 
-    private  void decode(){
+    private void decode(){
         codeStrategy.encode(this.text);
     }
 
