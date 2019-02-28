1d75f62b50ad82e4e66dbf8acb2395d02c6e5871
Unknown <sam.arnalsteen@gmail.com>
2019-02-28 15:24:01 +0100

    gitignore + mirrorStrategy

--- Geheimschrift/src/domain/MirrorStrategy.java
+++ Geheimschrift/src/domain/MirrorStrategy.java
@@ -3,11 +3,23 @@ package domain;
 public class MirrorStrategy implements CodeStrategy{
     @Override
     public String encode(String string) {
-        return null;
+        byte [] stringAsBytes = string.getBytes();
+        byte [] result = new byte[stringAsBytes.length];
+
+        for(int i = 0; i<stringAsBytes.length; i++){
+            result[i] = stringAsBytes[stringAsBytes.length - i - 1];
+        }
+        return new String(result);
     }
 
     @Override
     public String decode(String string) {
-        return null;
+        byte [] stringAsBytes = string.getBytes();
+        byte [] result = new byte[stringAsBytes.length];
+
+        for(int i = 0; i<stringAsBytes.length; i++){
+            result[i] = stringAsBytes[stringAsBytes.length - i - 1];
+        }
+        return new String(result);
     }
 }
