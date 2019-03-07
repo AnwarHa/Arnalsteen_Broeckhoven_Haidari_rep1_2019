c3433af9e85156b09050bcafb8354adcf9924fa7
Kalimath <mathieu_broe@yahoo.com>
2019-03-07 15:59:59 +0100

    FunforRentui methods and showMenu implemented

--- FunForRent/src/ui/FunForRentUI.java
+++ FunForRent/src/ui/FunForRentUI.java
@@ -1,7 +1,67 @@
 package ui;
 
+import domain.RentContext;
+
+import javax.swing.*;
+
 public class FunForRentUI {
-    public static void main(String[] args) {
+    RentContext rentContext;
+
+    public FunForRentUI(RentContext rentContext) {
+        this.rentContext = rentContext;
+        showMenu();
+    }
+
+    private void showMenu() {
+        String menu = "1. Add article\n2. Rent article\n3. Repair article\n4.Return article\n5.Remove article\n6. Show available articles\n\n0. Quit app";
+        int choice = -1;
+        while (choice != 0) {
+            String choiceString = JOptionPane.showInputDialog(menu);
+            try {
+                choice = Integer.parseInt(choiceString);
+            }catch (NumberFormatException e){
+                choice =-1;
+            }
+            switch (choice) {
+                case 1:
+                    addArticle();
+                    break;
+                case 2:
+                    rentArticle();
+                    break;
+                case 3:
+                    repairArticle();
+                    break;
+                case 4:
+                    returnArticle();
+                    break;
+                case 5:
+                    removeArticle();
+                    break;
+                case 6:
+                    showAvailableArticles();
+                    break;
+            }
+        }
+    }
+
+    private void showAvailableArticles() {
+        
+    }
+
+    private void returnArticle() {
+    }
+
+    private void repairArticle() {
+    }
+
+    private void rentArticle() {
+    }
+
+    private void addArticle() {
+    }
+
+    public void removeArticle(){
 
     }
 }
