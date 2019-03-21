25289e4d5e22824699ff05f9f4919aff3a88dd77
Kalimath <mathieu_broe@yahoo.com>
2019-03-21 17:22:58 +0100

    scoreboard en throwdice ongev af

--- Dobbelspel/src/domain/Game.java
+++ Dobbelspel/src/domain/Game.java
@@ -3,11 +3,7 @@ package domain;
 import java.util.*;
 
 public class Game {
-    private ScoreBoard player1;
-    private ScoreBoard player2;
-    private ScoreBoard player3;
-    private int turns 4;
-    private ArrayList orderTurns;
+
 
     public Game() {
 
--- Dobbelspel/src/domain/GameObservable.java
+++ Dobbelspel/src/domain/GameObservable.java
@@ -1,6 +1,46 @@
 package domain;
 
-import javafx.beans.Observable;
+import java.util.Observable;
 
-public class GameObservable implements Observable {
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class GameObservable extends Observable {
+    private ScoreBoard player1;
+    private ScoreBoard player2;
+    private ScoreBoard player3;
+    private int turns = 4;
+    private ArrayList<ScoreBoard> players;
+    private int currentPlayer;
+    private int currentRound=1;
+
+    public GameObservable() {
+        players = new ArrayList();
+        player1 = new ScoreBoard();
+        player2 = new ScoreBoard();
+        player3 = new ScoreBoard();
+        players.add(player1);
+        players.add(player2);
+        players.add(player3);
+    }
+
+    public void nextRound(){
+        currentRound++;
+    }
+
+    public int getCurrentPlayer(){
+        return currentPlayer;
+    }
+
+    public int getNextPlayer(){
+        return currentPlayer++;
+    }
+
+    public void throwDice(){
+        players.get(this.getCurrentPlayer()-1).setScore(ThreadLocalRandom.current().nextInt(2, 12 + 1));
+    }
+
+
+
 }
